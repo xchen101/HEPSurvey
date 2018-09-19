4abc82b4087e3b2c238bbbe61500a4c53e9deac7
Xiaoli Chen <xiaoli.chen@cern.ch>
2018-09-19 14:38:49 +0200

    add stacked split bar chart for factors stats,

--- R/likert_plot.R
+++ R/likert_plot.R
@@ -8,17 +8,55 @@ library(dplyr)
 library(ggthemes)
 library(stringr)
 
-coded_data <- read_csv("Data/Base/Usable_QC_FA_coded.csv")
+factor <- read.csv("Data/Processed/factor1.csv")
 
-mytitle <- "\"What is your impression about Open Science?"
-mylevels <- c("Strongly disagree", "Somewhat disagree", "Neutral", "Somewhat agree", "Strongly agree")
+mytitle <- "Factors that affect sharing decision"
+mylevels <- c("Not at all affect", "Rarely affect", "Neutral", "Somewhat affect", "Affect a lot")
 
-OS1 <- coded_data %>%
-  filter(!is.na(Sex)) %>%
-  group_by(Sex, D3, `p2q1 [OS1]`) %>% #<- change  
-  summarize(count = n()) %>%
-  mutate(freq = count / sum(count))
+numlevels<-length(factor[1,])-1
+numcenter<-ceiling(numlevels/2)+1
+factor$midvalues<-factor[,numcenter]/2
+factor2<-cbind(factor[,1],factor[,2:ceiling(numlevels/2)],
+            factor$midvalues,factor$midvalues,factor[,numcenter:numlevels+1])
+colnames(factor2)<-c("outcome",mylevels[1:floor(numlevels/2)],"midlow",
+                  "midhigh",mylevels[numcenter:numlevels])
 
-OS1
+numlevels<-length(mylevels)+1
+point1<-2
+point2<-((numlevels)/2)+1
+point3<-point2+1
+point4<-numlevels+1
+mymin<-(ceiling(max(rowSums(factor2[,point1:point2]))*4)/4)*-100
+mymax<-(ceiling(max(rowSums(factor2[,point3:point4]))*4)/4)*100
 
-numlevels <- length()
+numlevels<-length(factor[1,])-1
+temp.rows<-length(factor2[,1])
+pal<-brewer.pal((numlevels-1),"RdBu")
+pal[ceiling(numlevels/2)]<-"#DFDFDF"
+legend.pal<-pal
+pal<-c(pal[1:(ceiling(numlevels/2)-1)], pal[ceiling(numlevels/2)], 
+       pal[ceiling(numlevels/2)], pal[(ceiling(numlevels/2)+1):(numlevels-1)])
+
+factor3<-melt(factor2,id="outcome")
+factor3$col<-rep(pal,each=temp.rows)
+factor3$value<-factor3$value*100
+factor3$outcome<-str_wrap(factor3$outcome, width = 40)
+factor3$outcome<-factor(factor3$outcome, levels = factor2$outcome[order(-(factor2[,5]+factor2[,6]+factor2[,7]))])
+highs<-na.omit(factor3[(length(factor3[,1])/2)+1:length(factor3[,1]),])
+lows<-na.omit(factor3[1:(length(factor3[,1])/2),])
+lows <- lows[rev(rownames(lows)),]
+lows$col <- factor(lows$col, levels = c("#CA0020","#F4A582", "#DFDFDF"))
+
+ggplot() + 
+  geom_bar(data=highs, aes(x = outcome, y=value, fill=col), position="stack", stat="identity") +
+  geom_bar(data=lows, aes(x = outcome, y=-value, fill=col), position="stack", stat="identity") +
+  geom_hline(yintercept = 0, color =c("white")) +
+  scale_fill_identity("Percent", labels = mylevels, breaks=legend.pal, guide="legend") + 
+  theme_fivethirtyeight() + 
+  coord_flip() +
+  labs(title=mytitle, y="",x="") +
+  theme(plot.title = element_text(size=14, hjust=0.5)) +
+  theme(axis.text.y = element_text(hjust=0)) +
+  theme(legend.position = "bottom") +
+  scale_y_continuous(breaks=seq(mymin,mymax,25), limits=c(mymin,mymax))
+
