90a6943296eca7796c40ac6207800c4b7cc8ccdf
Xiaoli Chen <xiaoli.chen@cern.ch>
2018-10-25 18:25:23 +0200

    clustering tests

--- clusting_test.R
+++ clusting_test.R
@@ -1,17 +1,5 @@
 library(FactoMineR)
 library(factoextra)
-# Compute PCA with ncp = 3
-res.pca <- PCA(USArrests, ncp = 3, graph = FALSE)
-# Compute hierarchical clustering on principal components
-res.hcpc <- HCPC(res.pca, graph = FALSE)
-fviz_dend(res.hcpc, 
-          cex = 0.7,                     # Label size
-          palette = "jco",               # Color palette see ?ggpubr::ggpar
-          rect = TRUE, rect_fill = TRUE, # Add rectangle around groups
-          rect_border = "jco",           # Rectangle color
-          labels_track_height = 0.8      # Augment the room for labels
-)
-
 
 clusterTest <- read_csv("Data/Processed/clusteringTest.csv")
 res.pca <- PCA(clusterTest, ncp = 3, graph = FALSE)
@@ -62,11 +50,11 @@ clusterTest2 <- read_csv("Data/Processed/clusteringTest2.csv")
 data(clusterTest2)
 res.mca <- MCA(clusterTest2,
                ncp = 20,
-               graph = FALSE)
-res.hcpc <- HCPC(res.mca, graph = FALSE, max = 4)
+               graph = TRUE)
+res.hcpc <- HCPC(res.mca, nb.clust = -1, min = 3, max = NULL, graph = T)
 fviz_dend(res.hcpc, show_labels = FALSE)
 fviz_cluster(res.hcpc, geom = "point", main = "Factor map")
-# Description by variables
+h# Description by variables
 res.hcpc$desc.var$test.chi2
 # Description by variable categories
 res.hcpc$desc.var$category
@@ -74,4 +62,30 @@ res.hcpc$desc.var$category
 res.hcpc$desc.axes
 # Description by individuals
 res.hcpc$desc.ind$para
+
+
+
+test.mca <- MCA(clusterTest2,
+               ncp = 20,
+               graph = T)
+test.hcpc <- HCPC(test.mca, nb.clust = -1, graph = FALSE)
+fviz_dend(res.hcpc, show_labels = FALSE)
+fviz_cluster(test.hcpc, geom = "point", main = "Factor map")
+# Description by variables
+res.hcpc$desc.var$test.chi2
+# Description by variable categories
+res.hcpc$desc.var$category
+# show principal dimensions that are the most associated with clusters
+res.hcpc$desc.axes$quanti
+# Description by individuals
+res.hcpc$desc.ind$para
+
+dimdesc(res.mca, axes=1:7, proba=0.05)
+clusterTest2 <- res.hcpc$data.clust
+
+
+
+clusterTest21 <- clusterTest2 %>%
+  group_by(clust) %>%
+  summarize(count = n()) 
 
